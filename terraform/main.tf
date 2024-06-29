388bd75bbfeea1cb58a9f669ce3d3cd969f06fb5
awsfreetier30 <awsfreetier30@gmail.com>
2024-06-29 16:18:43 +0530

    update -100

---
+++ terraform/main.tf
@@ -0,0 +1,127 @@
+# terraform/main.tf
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "main-subnet"
+  }
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "main-route-table"
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_security_group" "web" {
+  name        = "allow_web_traffic"
+  description = "Allow inbound web traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+
+  subnet_id                   = aws_subnet.main.id
+  vpc_security_group_ids      = [aws_security_group.web.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update
+              apt upgrade -y
+              apt install -y apache2
+              a2enmod rewrite
+              cat > /etc/apache2/sites-available/000-default.conf <<EOT
+              <VirtualHost *:80>
+                  ServerAdmin webmaster@localhost
+                  DocumentRoot /var/www/html
+                  <Directory /var/www/html>
+                      Options Indexes FollowSymLinks
+                      AllowOverride All
+                      Require all granted
+                  </Directory>
+                  ErrorLog \${APACHE_LOG_DIR}/error.log
+                  CustomLog \${APACHE_LOG_DIR}/access.log combined
+              </VirtualHost>
+              EOT
+              chown -R ubuntu:ubuntu /var/www/html
+              chmod -R 755 /var/www/html
+              usermod -a -G ubuntu www-data
+              systemctl restart apache2
+              EOF
+
+  tags = {
+    Name = "WebServer"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
---
+++ terraform/variable.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "The AWS region to create resources in"
+  default     = "us-east-1"
+}
+
+variable "ami_id" {
+  description = "The AMI ID for Ubuntu 22.04"
+  default     = "ami-0e001c9271cf7f3b9" # Ubuntu 22.04 LTS in us-east-1
+}
+
+variable "key_name" {
+  description = "The key pair name for SSH access"
+  default     = "key1" # This should match the name of your key pair in AWS, without the .pem extension
+}
